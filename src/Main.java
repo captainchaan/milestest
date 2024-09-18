73cea2d8647c8d0704f38dcba0cbd6f1557bbdc2
TEST <annivanova@list.ru>
2024-09-18 20:23:50 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13676;
+        int bonusPoints = 20;
+
+        int miles = ticket / bonusPoints;
+        System.out.println("Начисленно бонусных миль:" + miles);
+
+    }
+}
